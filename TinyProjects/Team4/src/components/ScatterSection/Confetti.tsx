7ea5e9f83d7872a250ab8d21550fbc17958bf87f
wooooooood <13pft13@gmail.com>
2022-05-29 23:12:57 +0900

    refactor

--- TinyProjects/Team4/src/components/ScatterSection/Confetti.tsx
+++ TinyProjects/Team4/src/components/ScatterSection/Confetti.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
 
 interface Props {
   children?: JSX.Element[];
@@ -12,17 +11,17 @@ interface IConfetti {
 
 export function Confetti(): React.ReactElement {
   const elRef = useRef<HTMLDivElement>(null);
-  const confettiColors = ['#ffc400', '#ff726d', '#b48def', '#f4306d', '#007bff'];
+  const confettiColors = ['#ffc400', '#ff726d', '#b48def', '#f4306d', '#007bff', '#49c44b'];
   const [confetties, setConfetties] = useState<Array<IConfetti>>([]);
 
   useEffect(() => {
     setInterval(() => {
       if (!elRef.current) return;
 
-      const confettiSize = `${Math.floor(Math.random() * 4) + 10}px`;
+      const confettiSize = `${Math.floor(Math.random() * 4) + 12}px`;
       const confettiBackground = confettiColors[Math.floor(Math.random() * confettiColors.length)];
       const confettiLeft = `${Math.floor(Math.random() * elRef.current.offsetWidth)}px`;
-      const confettiShape = Math.floor(Math.random() * 10) / 2 === 0 ? 'confetti' : 'confetti-round';
+      const confettiShape = Math.floor(Math.random() * 10) / 2 === 0 ? '' : 'round';
 
       const inlineStyles = {
         left: confettiLeft,
@@ -31,10 +30,7 @@ export function Confetti(): React.ReactElement {
         backgroundColor: confettiBackground,
       };
 
-      setConfetties((confetties) => {
-        console.log([...confetties, { style: inlineStyles, shape: confettiShape }]);
-        return [...confetties, { style: inlineStyles, shape: confettiShape }];
-      });
+      setConfetties((confetties) => [...confetties, { style: inlineStyles, shape: confettiShape }]);
     }, 4000);
   }, []);
 
@@ -42,22 +38,7 @@ export function Confetti(): React.ReactElement {
     <Wrapper ref={elRef}>
       <div id="confetti-container">
         {confetties.map((item, key) => (
-          <motion.div
-            key={key}
-            // animate={{
-            //   rotateX: [0, 360],
-            //   rotateY: [0, 180],
-            //   translateX: [0, 0],
-            //   translateY: [0, 100],
-            // }}
-            transition={{
-              duration: 2.5,
-              ease: 'linear',
-              repeat: Infinity,
-            }}
-            style={item.style}
-            className={`confetti--animation-slow ${item.shape}`}
-          />
+          <div key={key} style={item.style} className={`confetti--animation-slow confetti ${item.shape}`} />
         ))}
       </div>
     </Wrapper>
@@ -99,15 +80,7 @@ const Wrapper = styled.div`
     }
   }
 
-  .confetti-round {
-    position: absolute;
-    z-index: 1;
-    top: -10px;
+  .round {
     border-radius: 50%;
-
-    &--animation-slow {
-      animation: confetti-slow 2.25s linear 1 forwards;
-      animation-iteration-count: infinite;
-    }
   }
 `;
--- TinyProjects/Team4/src/components/ScatterSection/index.tsx
+++ TinyProjects/Team4/src/components/ScatterSection/index.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
 import { Confetti } from './Confetti';
+import { Heart } from './Heart';
 
 /**
  *
@@ -26,23 +27,7 @@ export function ScatterSection(): React.ReactElement {
         </motion.p>
       </div>
       <div className="thanksto--container">
-        <div className="heart-wrapper">
-          <motion.svg
-            animate={{
-              scale: [1, 2, 1],
-              opacity: [0.8, 0.4, 0.8],
-            }}
-            transition={{
-              duration: 1.5,
-              ease: 'easeInOut',
-              repeat: Infinity,
-            }}
-            className="heart"
-            viewBox="0 0 32 32"
-          >
-            <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" />
-          </motion.svg>
-        </div>
+        <Heart />
         <div className="content">
           <motion.h3 whileInView={{ color: '#fff' }}>Special Thanks to</motion.h3>
           <a target="_blank" rel="noopener noreferrer" href="https://github.com/febase">
@@ -55,6 +40,7 @@ export function ScatterSection(): React.ReactElement {
           </motion.p>
         </div>
       </div>
+
       <div className="members--container">
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/walt1992">
           <div className="members--item">
@@ -93,7 +79,6 @@ const Section = styled(motion.section)`
     font-size: 5rem;
     margin: 1.5rem 0 2.25rem;
   }
-
   h2 {
     font-size: 3.25rem;
   }
@@ -111,88 +96,24 @@ const Section = styled(motion.section)`
     margin: 120px 0;
     position: relative;
   }
-  .thanksto--container .heart-wrapper {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    width: 100px;
-    height: 100px;
-  }
-  .heart {
-    fill: rgba(204, 42, 93, 1);
-    width: 100%;
-    height: 100%;
-    z-index: 0;
-  }
   .thanksto--container .content {
     position: relative;
   }
 
   .members--container {
+    position: relative;
     display: flex;
     gap: 8px;
     justify-content: center;
   }
-
   .members--item {
     width: 200px;
   }
-
   .name {
     color: white;
     font-weight: bold;
   }
   .role {
     color: #ccc;
-  }
-
-  @keyframes confetti-slow {
-    0% {
-      transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
-    }
-    100% {
-      transform: translate3d(25px, 105vh, 0) rotateX(360deg) rotateY(180deg);
-    }
-  }
-  @keyframes confetti-medium {
-    0% {
-      transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
-    }
-    100% {
-      transform: translate3d(100px, 105vh, 0) rotateX(100deg) rotateY(360deg);
-    }
-  }
-  @keyframes confetti-fast {
-    0% {
-      transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
-    }
-    100% {
-      transform: translate3d(-50px, 105vh, 0) rotateX(10deg) rotateY(250deg);
-    }
-  }
-  .confetti-container {
-    perspective: 700px;
-    position: absolute;
-    overflow: hidden;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
-  .confetti {
-    position: absolute;
-    z-index: 1;
-    top: -10px;
-    border-radius: 0;
-  }
-  .confetti--animation-slow {
-    animation: confetti-slow 2.25s linear 1 forwards;
-  }
-  .confetti--animation-medium {
-    animation: confetti-medium 1.75s linear 1 forwards;
-  }
-  .confetti--animation-fast {
-    animation: confetti-fast 1.25s linear 1 forwards;
   }
 `;
